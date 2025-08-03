288730d717f5c59008e507ba89d3d427dde71172
Sripriyanka <usripriyanka@gmail.com>
2025-08-03 19:21:35 +0530

    Create Asteroid collision.cpp

---
+++ Asteroid collision.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+        
+
+        for(int ast : asteroids){
+            bool destroyed=false;
+            while(!st.empty() && ast<0 && st.top()>0){
+                if(abs(ast)>st.top()){
+                    st.pop();
+                    continue;
+                }
+                else if(abs(ast)==st.top()){
+                    st.pop();
+                }
+                destroyed=true;
+                break;
+                }
+                if(!destroyed){
+                    st.push(ast);
+                }
+            }
+     
+            vector<int> res(st.size());
+            for(int i=st.size()-1;i>=0;i--){
+                res[i]=st.top();
+                st.pop();
+            }         
+            return res;   
+        
+    }
+};
